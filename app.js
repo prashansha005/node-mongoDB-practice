b6d71d250d78aca94f443cc18e2b159f2172fc3b
Prashansha Neupane <neupane.prashansha2005@gmail.com>
2026-01-04 16:39:07 +0545

    mongoose in express

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const model = require("./userModel");
+const User = require("./userModel");
 
 // app.get("/", (req, res) => {
 //   res.send("Hello world");
@@ -52,36 +52,40 @@ const model = require("./userModel");
 // });
 // app.listen(3000);
 
+app.get("/", (req, res) => {
+  res.send("hello!!!!!!");
+});
+
+app.post("/create", async (req, res) => {
+  const user = await User.create(req.body);
+  res.send(user);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on 3000");
+});
+
+//const u=new user(req.body);//creates an object
+// await u.save();
+
+// app.use(express.urlencoded({ extended: true })); //HTML form support
+
 // app.get("/", (req, res) => {
-//   res.send("hello!!!!!!");
+//   res.send(`
+//       <form action="/create" method="POST">
+//          <input type="text" name="name" placeholder="Name"/>
+//          <input type="email" name="email" placeholder="Email"/>
+//          <input type="text" name="nickname" placeholder="Nickname"/>
+//          <button type="submit">submit</button>
+//       </form>
+//    `);
 // });
 
 // app.post("/create", async (req, res) => {
-//   const user = await user.create(req.body);
-//   res.send(user);
+//   const user = await User.create(req.body);
+//   res.send(`User saved!Name:${user.name},Email:${user.email}`);
 // });
 
 // app.listen(3000, () => {
 //   console.log("Server running on 3000");
 // });
-
-//const u=new user(req.body);//creates an object
-// await u.save();
-
-app.use(express.urlencoded({ extended: true })); //HTML form support
-
-app.get("/", (req, res) => {
-  res.send(`
-      <form action="/create" method="POST">
-         <input type="text" name="name" placeholder="Name"/>
-         <input type="email" name="email" placeholder="Email"/>
-         <input type="text" name="nickname" placeholder="Nickname"/>
-         <button type="submit">submit</button>
-      </form>
-   `);
-});
-
-app.post("/create", async (req, res) => {
-  const user = await UserActivation.create(req.body);
-  res.send(`User saved!Name:${user.name},Email:${user.email}`);
-});
